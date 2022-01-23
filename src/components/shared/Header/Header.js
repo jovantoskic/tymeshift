3d5ce68245f8db50caaf760bdf7f5330a9cc9c8b
Jovan Toskic <jovan@Jovans-MacBook-Pro.local>
2022-01-23 16:01:04 +0100

    Add header component styles

---
+++ src/components/shared/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p className="all-locations">All locations</p>
+      <h2 className="acme-locations">Acme locations</h2>
+    </header>
+  );
+};
+
+export default Header;
