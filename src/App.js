0db793034c2af3f06598c9b3136d94597bc4e060
Jovan Toskic <jovan@Jovans-MacBook-Pro.local>
2022-01-25 15:50:56 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import AppRoutes from './routes';
 
 import './index.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <AppRoutes />
     </div>
   );
-}
+};
 
 export default App;
