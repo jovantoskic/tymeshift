cc7b91864d1deade2f68e00e15c68ed0a4a0ec9c
Jovan Toskic <jovan@Jovans-MacBook-Pro.local>
2022-01-25 15:40:41 +0100

    Add location structure and style

--- src/components/pages/Locations/Locations.js
+++ src/components/pages/Locations/Locations.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getLocations } from 'api/locations';
+import useFetch from 'hooks/useApi';
 
 import Card from 'components/shared/Card';
 import Layout from 'components/shared/Layout';
+import Loading from 'components/shared/Loading';
+
+import './Locations.scss';
 
 const Locations = () => {
-  return (
+  const locations = useFetch(getLocations);
+
+  useEffect(() => {
+    locations.request();
+  }, []);
+
+  const { data, loading } = locations;
+
+  return !loading ? (
     <Layout>
-      <Card />
+      <section className="locations-container">
+        {data &&
+          data.map(location => (
+            <Card
+              key={location.id}
+              users={location.userCount}
+              time={location.createdAt}
+              description={location.description}
+            />
+          ))}
+      </section>
     </Layout>
+  ) : (
+    <Loading />
   );
 };
 
